2da987b5d5e6432797e42ca16a38d462f58ca907
Chayse Stiver <cmstiver1@gmail.com>
2021-07-05 15:11:00 -0700

    minor change

--- src/home.js
+++ src/home.js
@@ -7,7 +7,7 @@ function createHome() {
     content.appendChild(intro)
     let about = document.createElement('div')
     about.classList = 'item'
-    about.innerHTML += `This project helped me practice my skills with WebPack! You can find the source code <a href="https://github.com/cmstiver/restaurant-page">here</a>! Also, thanks to <a href="https://www.flaticon.com/search?word=cartoon%20cat&type=icon">flaticon</a> for the cat logo.`
+    about.innerHTML += `This project helped me practice my skills with WebPack! You can find the source code <a href="https://github.com/cmstiver/restaurant-page">here</a>! Also, thanks to <a href="https://www.flaticon.com/free-icon/pussy-cat-cartoon-outline-variant_29662?term=cartoon%20cat&page=1&position=4&page=1&position=4&related_id=29662&origin=search">Freepik on Flaticon</a> for the cat logo.`
     content.appendChild(about)
 }
 
