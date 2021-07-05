e1412e8ee5511927877bb80e294ab57b965457ab
Chayse Stiver <cmstiver1@gmail.com>
2021-07-05 02:51:27 -0700

    created contacts

---
+++ src/contacts.js
@@ -0,0 +1,9 @@
+
+function createContacts() {
+    let intro = document.createElement('div')
+    intro.classList = 'item'
+    intro.textContent = "You can reach us at 555-555-5555"
+    content.appendChild(intro)
+}
+
+export default createContacts;
